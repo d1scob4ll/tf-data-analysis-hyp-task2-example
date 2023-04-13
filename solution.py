a57ebe74328017504f0c44e15563cfbcab94381a
d1scob4ll <130067358+d1scob4ll@users.noreply.github.com>
2023-04-13 23:17:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,21 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 305011093 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.08
+    xm=x.mean()
+    ym=y.mean()
+    xd=x.var()
+    yd=y.var()
+
+    ans=abs((xm-ym)/(np.sqrt(xd+yd)))
+    result = True
+    if ans < alpha:
+      result = False
+    
+    return result # Ваш ответ, True или False
